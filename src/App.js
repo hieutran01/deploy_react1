936f90ad1e98c1717e95bcd8a4bf99b7a28b2f84
hieutran01 <trandinhhieu19012002@gmail.com>
2023-08-10 11:24:32 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { useEffect, useState } from "react";
 import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
@@ -13,28 +12,19 @@ import TourShow from "./pages/TourShow";
 import './App.css';
 
 function App() {
- 
- 
-
-
   return (
     <div className="App">
-
       <div className="content">
-
         <Provider store={store}>
-        <Routes>
-
-          <Route path="/tours" element={<TourList/>} />
-          <Route path="" element={<TourList/>} />
-          <Route path="/tours/create" element={<TourCreate />} />
-          <Route path="/tours/delete/:id" element={<TourDelete />} />
-          <Route path="/tours/:id/edit" element={<TourEdit />} />
-          <Route path="/tours/:id" element={<TourShow />} />
-        </Routes>
+            <Routes>
+              <Route path="deploy_react1/*" element={<TourList />} />
+              <Route path="deploy_react1/tours/create" element={<TourCreate />} />
+              <Route path="deploy_react1/tours/delete/:id" element={<TourDelete />} />
+              <Route path="deploy_react1/tours/:id/edit" element={<TourEdit />} />
+              <Route path="deploy_react1/tours/:id" element={<TourShow />} />
+            </Routes>
         </Provider>
       </div>
-    
     </div>
   );
 }
